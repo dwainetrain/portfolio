1e251f422ce151c36448df199dd2682e14a18a73
Dwaine Best <dwaine.best@gmail.com>
2020-11-03 06:49:23 -0800

    Contentful abandoned due to unresolved connection problems. Went with my second choice, using GraphCMS instead. Connection established and data queried successfully.

---
+++ gatsby-node.js
@@ -0,0 +1,26 @@
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+  const {
+    data: {
+      gcms: { pages },
+    },
+  } = await graphql(`
+    {
+      gcms {
+        pages(stage: PUBLISHED) {
+          id
+          slug
+        }
+      }
+    }
+  `);
+
+  pages.forEach(({ id, slug }) =>
+    createPage({
+      path: `/pages/${slug}`,
+      component: require.resolve("./src/templates/Page.js"),
+      context: {
+        id,
+      },
+    })
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
+import Layout from "components/Layout";
+import Container from "components/Container";
 
-import img_gatsby from 'assets/images/gatsby-astronaut.png';
+import img_gatsby from "assets/images/gatsby-astronaut.png";
+
+// GraphCMS Connection
+import { graphql, useStaticQuery, Link } from "gatsby";
+
+const pageQuery = graphql`
+  {
+    gcms {
+      pages {
+        id
+        title
+        slug
+        body
+      }
+    }
+  }
+`;
 
 const IndexPage = () => {
+  const {
+    gcms: { pages },
+  } = useStaticQuery(pageQuery);
+
   return (
     <Layout pageName="home">
       <Helmet>
@@ -16,20 +36,23 @@ const IndexPage = () => {
         <p className="gatsby-astronaut">
           <img src={img_gatsby} alt="Build with Gatsby!" />
         </p>
+        <p>Testing GraphCMS Conntection:</p>
+        <div>
+          {pages.map(({slug, ...pages}) => (
+            <Link key={slug} to={`/pages/${slug}`}>
+              {pages.title}
+            </Link>
+          ))}
+        </div>
         <h1>Gatsby Sass Starter</h1>
-        <p>
-          Welcome to your new Gatsby site.
-        </p>
-        <p>
-          Now go build something great.
-        </p>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
         <h2>Still Getting Started?</h2>
-        <p>
-          Run the following in your terminal!
-        </p>
+        <p>Run the following in your terminal!</p>
         <pre>
           <code>
-            gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-sass
+            gatsby new [directory]
+            https://github.com/colbyfayock/gatsby-starter-sass
           </code>
         </pre>
       </Container>
